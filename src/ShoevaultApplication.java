ec96ac9e7879e75a25fcc8d0a82d31b22872e5e8
14704 <nahumtad123@gmail.com>
2025-04-22 14:01:05 -0400

    Initial commit for this Project

---
+++ src/ShoevaultApplication.java
@@ -0,0 +1,12 @@
+package com.shoevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoevaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoevaultApplication.class, args);
+    }
+}
